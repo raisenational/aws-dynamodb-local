c08bd521f7b7d6b0d48fc084225faabf07bd211b
Adam Jones <domdomegg+git@gmail.com>
2023-10-15 17:06:48 +0100

    TypeScript rewrite (#5)

---
+++ src/stop.ts
@@ -0,0 +1,9 @@
+import { instances } from './instances';
+
+export const stop = async (port: number): Promise<void> => {
+  const instance = instances.get(port);
+  if (instance) {
+    instance.process.kill('SIGKILL');
+    instances.delete(port);
+  }
+};
